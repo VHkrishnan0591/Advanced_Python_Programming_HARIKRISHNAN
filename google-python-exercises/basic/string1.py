915a3ab5c8496c671e7ad09b771908f1d9669a58
VHkrishnan0591 <harikrishnanv0591@gmail.com>
2024-03-25 00:01:57 +0530

    The code change will complete the task in string1.py file

--- google-python-exercises/basic/string1.py
+++ google-python-exercises/basic/string1.py
@@ -25,7 +25,9 @@
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
   # +++your code here+++
-  return
+  if count<10:
+    return "Number of donuts: %d" %(count)
+  else: return "Number of donuts: many"
 
 
 # B. both_ends
@@ -35,7 +37,9 @@ def donuts(count):
 # is less than 2, return instead the empty string.
 def both_ends(s):
   # +++your code here+++
-  return
+  if len(s) >2:
+    return s[:2] + s[-2:]
+  else: return ""
 
 
 # C. fix_start
@@ -49,7 +53,8 @@ def both_ends(s):
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
   # +++your code here+++
-  return
+  m = s[0] + s[1:].replace(s[0], "*")
+  return m
 
 
 # D. MixUp
@@ -61,7 +66,9 @@ def fix_start(s):
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
   # +++your code here+++
-  return
+  c = a.replace(a[:2],b[:2])
+  b = b.replace(b[:2],a[:2])
+  return c + " " + b
 
 
 # Provided simple test() function used in main() to print
